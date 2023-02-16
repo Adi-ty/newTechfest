35bb839eb7fb4b1ee496094dbdf9aa75b1fe5a91
Aditya <adityasinghboss1234@gmail.com>
2023-02-16 13:59:39 +0530

    Added commonly used motions and gradients

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,7 +1,16 @@
+"use client";
+
+import { motion } from "framer-motion";
+import { navVariants } from "@/utils/motion";
+
 import React from "react";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <motion.nav variants={navVariants} initial="hidden" whileInView="show">
+      Navbar
+    </motion.nav>
+  );
 };
 
 export default Navbar;
