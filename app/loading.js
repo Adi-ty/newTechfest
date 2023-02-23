8b40181ea399e7d34f603fda6a05e483dceb8665
Aditya <adityasinghboss1234@gmail.com>
2023-02-23 21:47:55 +0530

    added loader

--- app/loading.js
+++ app/loading.js
@@ -1,7 +1,37 @@
 import React from "react";
+import "../styles/loading.css";
 
 const Loading = () => {
-  return <div>Loading...</div>;
+  return (
+    <div>
+      <div class="boxes">
+        <div class="box">
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+        <div class="box">
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+        <div class="box">
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+        <div class="box">
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Loading;
--- sections/Hero.jsx
+++ sections/Hero.jsx
@@ -1,49 +1,22 @@
 "use client";
 
 import { motion } from "framer-motion";
-import React, { useState, useEffect } from "react";
-import { slideIn } from "../utils/motion";
+import React from "react";
+import { staggerContainer } from "../utils/motion";
+import { TitleText } from "../components/CustomTexts";
+import styles from "../styles";
 
 const Hero = () => {
-  const [heading, setHeading] = useState("");
-  const headings = [
-    "Welcome",
-    "Hello",
-    "Bonjour",
-    "Hola",
-    "Ciao",
-    "Konnichiwa",
-  ];
-
-  useEffect(() => {
-    let count = 0;
-    const interval = setInterval(() => {
-      setHeading(headings[count]);
-      count = (count + 1) % headings.length;
-    }, 2000);
-    return () => clearInterval(interval);
-  }, []);
-
   return (
     <section>
       <motion.div
-        variants={slideIn("right", "tween", 0.2, 1)}
-        className="relative h-screen"
+        variants={staggerContainer}
+        initial="hidden"
+        whileInView="show"
+        viewport={{ once: false, amount: 0.25 }}
+        className={`${styles.innerWidth} mx-auto ${styles.flexCenter} flex-col `}
       >
-        <div className="absolute inset-0">
-          <div className="h-full w-full bg-gradient-to-r from-purple-400 via-pink-500 to-red-500" />
-        </div>
-        <div className="absolute inset-0 flex justify-center items-center">
-          <div className="text-5xl font-bold text-white">
-            <motion.span
-              initial={{ opacity: 0 }}
-              animate={{ opacity: 1 }}
-              transition={{ delay: 1 }}
-            >
-              {heading}
-            </motion.span>
-          </div>
-        </div>
+        <TitleText title={<>Elaysian'23</>} textStyles="text-center" />
       </motion.div>
     </section>
   );
