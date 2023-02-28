d8ca2d6ae0d0de25f71d08037b25bbf24bfc7fca
Aditya <adityasinghboss1234@gmail.com>
2023-02-28 21:44:44 +0530

    minor changes

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,15 +1,15 @@
-'use client';
+"use client";
 import { motion, useScroll } from "framer-motion";
-import Link from 'next/link';
-import React, { useState, useEffect } from 'react';
-import { AiOutlineMenu, AiOutlineClose } from 'react-icons/ai';
+import Link from "next/link";
+import React, { useState, useEffect } from "react";
+import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
 import "../styles";
 import "../styles/Hero.css";
 
 const Navbar = () => {
   const [nav, setNav] = useState(false);
-  const [color, setColor] = useState('transparent');
-  const [textColor, setTextColor] = useState('white');
+  const [color, setColor] = useState("transparent");
+  const [textColor, setTextColor] = useState("white");
 
   const handleNav = () => {
     setNav(!nav);
@@ -18,69 +18,73 @@ const Navbar = () => {
   useEffect(() => {
     const changeColor = () => {
       if (window.scrollY >= 90) {
-        setColor('#ffffff');
-        setTextColor('#000000');
+        setColor("#ffffff");
+        setTextColor("#000000");
       } else {
-        setColor('transparent');
-        setTextColor('#ffffff');
+        setColor("transparent");
+        setTextColor("#ffffff");
       }
     };
-    window.addEventListener('scroll', changeColor);
+    window.addEventListener("scroll", changeColor);
   }, []);
 
   return (
-
     <motion.div
-      style={{ 
+      style={{
         background: "rgba(28, 9, 85, 0.55)",
-        borderRadius:"5px",
-        boxShadow:"0 4px 30px rgba(0, 0, 0, 0.1)",
-        backdropFilter:"blur(6px)",
-    
-      
-
-
-
-        }}
-      className='fixed left-4 top-1 right-4 z-10 ease-in duration-300'
-      initial={{y:-300}}
+        borderRadius: "5px",
+        boxShadow: "0 4px 30px rgba(0, 0, 0, 0.1)",
+        backdropFilter: "blur(6px)",
+      }}
+      className="fixed left-4 top-1 right-4 z-10 ease-in duration-300"
+      initial={{ y: -300 }}
       animate={{
-        y:0,
-        
+        y: 0,
       }}
       transition={{
-        duration:2,
-        delay:3.5,
-        ease:"easeIn"
+        duration: 2,
+        delay: 3.5,
+        ease: "easeIn",
       }}
     >
-    <div className="z-20" />
-      <div className='max-w-[1240px] m-auto flex justify-between items-center p-4 text-white'>
-        <Link href='/'>
-          <h1 style={{ color: `${textColor}` }} className='font-bold text-3xl font' >
-          Alasiyan
+      <div className="z-20" />
+      <div className="max-w-[1240px] m-auto flex justify-between items-center p-4 text-white">
+        <Link href="/">
+          <h1
+            style={{ color: `${textColor}` }}
+            className="font-bold text-3xl font"
+          >
+            Elasiyan
           </h1>
         </Link>
-        <ul style={{ color: `${textColor}` }} className='nav-btn hidden sm:flex'>
-          <li className='p-4'>
-            <Link href='/' className='active a-btn'>Home</Link>
+        <ul
+          style={{ color: `${textColor}` }}
+          className="nav-btn hidden sm:flex"
+        >
+          <li className="p-4">
+            <Link href="/events" className="a-btn">
+              Events
+            </Link>
           </li>
-          <li className='p-4'>
-            <Link href='/contact' className='a-btn'>Contact Us</Link>
+          <li className="p-4">
+            <Link href="/hackathon" className="a-btn">
+              HackEDCode
+            </Link>
           </li>
-          <li className='p-4'>
-            <Link href='/events' className='a-btn'>Events</Link>
+          <li className="p-4">
+            <Link href="/team" className="a-btn">
+              Team
+            </Link>
           </li>
-          <li className='p-4'>
-            <Link href='/contact' className='a-btn'>Slysian</Link>
-          </li>
-          <li className='p-4'>
-            <Link href='/contact' className='a-btn'>Team</Link>
+          <li className="p-4">
+            <Link href="/contact" className="a-btn">
+              Contact Us
+            </Link>
           </li>
         </ul>
 
         {/* Mobile Button */}
-        <div onClick={handleNav} className='block sm:hidden z-10'>
+        <div onClick={handleNav} className="block sm:hidden z-10">
           {nav ? (
             <AiOutlineClose size={20} style={{ color: `${textColor}` }} />
           ) : (
@@ -91,22 +95,34 @@ const Navbar = () => {
         <div
           className={
             nav
-              ? 'sm:hidden absolute top-0 left-0 right-0 bottom-0 flex justify-center items-center w-full h-screen bg-black text-center ease-in duration-300'
-              : 'sm:hidden absolute top-0 left-[-100%] right-0 bottom-0 flex justify-center items-center w-full h-screen bg-black text-center ease-in duration-300'
+              ? "sm:hidden absolute top-0 left-0 right-0 bottom-0 flex justify-center items-center w-full h-screen bg-black text-center ease-in duration-300"
+              : "sm:hidden absolute top-0 left-[-100%] right-0 bottom-0 flex justify-center items-center w-full h-screen bg-black text-center ease-in duration-300"
           }
         >
           <ul>
-            <li onClick={handleNav} className='p-4 text-4xl hover:text-gray-500'>
-              <Link href='/'>Home</Link>
+            <li
+              onClick={handleNav}
+              className="p-4 text-4xl hover:text-gray-500"
+            >
+              <Link href="/">Home</Link>
             </li>
-            <li onClick={handleNav} className='p-4 text-4xl hover:text-gray-500'>
-              <Link href='/#gallery'>Gallery</Link>
+            <li
+              onClick={handleNav}
+              className="p-4 text-4xl hover:text-gray-500"
+            >
+              <Link href="/#gallery">Gallery</Link>
             </li>
-            <li onClick={handleNav} className='p-4 text-4xl hover:text-gray-500'>
-              <Link href='/work'>Work</Link>
+            <li
+              onClick={handleNav}
+              className="p-4 text-4xl hover:text-gray-500"
+            >
+              <Link href="/work">Work</Link>
             </li>
-            <li onClick={handleNav} className='p-4 text-4xl hover:text-gray-500'>
-              <Link href='/contact'>Contact</Link>
+            <li
+              onClick={handleNav}
+              className="p-4 text-4xl hover:text-gray-500"
+            >
+              <Link href="/contact">Contact</Link>
             </li>
           </ul>
         </div>
--- sections/Hero.jsx
+++ sections/Hero.jsx
@@ -93,15 +93,13 @@ const Hero = () => {
                 animate={{ opacity: 1 }}
                 transition={{ delay: 5 }}
               >
-                <h2 className="text-white font "  >Alasiyan'23</h2>
-                <h5 className="text-white font size "  >The Tech Never Submerges</h5>
-                
+                <h2 className="text-white font ">Elasiyan'23</h2>
+                <h5 className="text-white font size ">
+                  The Tech Never Submerges
+                </h5>
               </motion.span>
-             
             </div>
-            
           </div>
-    
         </h2>
 
         {/* 
@@ -110,13 +108,12 @@ const Hero = () => {
       <img src="/mountains_front.png" id="m3" /> */}
 
         <img src="/lamp.png" id="man" />
-        
+
         <img src="/lamp1.png" id="p2" />
         {/* <img src="/bench.png" id="g1" /> */}
-         <img src="/bottom.png" id="g1" /> 
-         <img src="/lamp.png" id="man" />
-         <img src="/lamp1.png" id="p1" />
-        
+        <img src="/bottom.png" id="g1" />
+        <img src="/lamp.png" id="man" />
+        <img src="/lamp1.png" id="p1" />
 
         <motion.img
           src="/1234.jpg"
@@ -124,12 +121,25 @@ const Hero = () => {
           initial={{ opacity: 1 }}
           animate={{ scale: 10, opacity: 0 }}
           transition={{
-            delay: 3, duration: 2, ease:"linear",
-            
+            delay: 3,
+            duration: 2,
+            ease: "linear",
           }}
         ></motion.img>
-        <motion.img src="/fog.png" id="" animate={{ x: 600, opacity: 0.3 }} initial={{ opacity: 1 }} transition={{ duration: 3 }} ></motion.img>
-        <motion.img src="/fog.png" id="" animate={{ x: -600, opacity: 0.3 }} initial={{ opacity: 1 }} transition={{ duration: 3 }} ></motion.img>
+        <motion.img
+          src="/fog.png"
+          id=""
+          animate={{ x: 600, opacity: 0.3 }}
+          initial={{ opacity: 1 }}
+          transition={{ duration: 3 }}
+        ></motion.img>
+        <motion.img
+          src="/fog.png"
+          id=""
+          animate={{ x: -600, opacity: 0.3 }}
+          initial={{ opacity: 1 }}
+          transition={{ duration: 3 }}
+        ></motion.img>
         <h2 id="text">
           {" "}
           <div className="absolute inset-0 flex justify-center items-center">
@@ -138,16 +148,11 @@ const Hero = () => {
                 initial={{ opacity: 1 }}
                 animate={{ opacity: 0, scale: 5 }}
                 transition={{ delay: 1, duration: 2 }}
-              >
-
-              </motion.span>
-              
+              ></motion.span>
             </div>
           </div>
         </h2>
-       
       </section>
-     
     </div>
 
     // <section className={`${styles.yPaddings} sm:pl-16 pl-6`}>
