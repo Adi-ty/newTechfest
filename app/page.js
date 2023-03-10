4659bc7dcbe7a176432ddf0df01844127700cdb1
Chaitanya Rai <Chaitanyarai899@gmail.com>
2023-03-10 22:30:24 +0530

    gif added on front page

--- app/page.js
+++ app/page.js
@@ -1,24 +1,43 @@
+"use client";
+import React, { useState, useEffect } from 'react';
 import About from "../sections/About";
 import Explore from "../sections/Explore";
 import Hero from "../sections/Hero";
 import Insights from "../sections/Insights";
 import WhatsNew from "../sections/WhatsNew";
+import logo from "../public/0001.gif";
 
-const Page = () => (
-  <div>
-    <Hero />
-    {/* <div className="relative ">
-      
-      <div className="gradient-03 z-0" />
-      <Explore />
+const Page = () => {
+  const [showGif, setShowGif] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowGif(false);
+    }, 5000);
+    return () => clearTimeout(timer);
+  }, []);
+
+  return (
+    <div>
+      {showGif && <img src={logo} alt=" GIF loading...." />}
+      {!showGif && (
+        <>
+          <Hero />
+          {/* <div className="relative ">
+            
+            <div className="gradient-03 z-0" />
+            <Explore />
+          </div>
+          <div className="relative">
+            <WhatsNew />
+            <div className="gradient-04 z-0" />
+            <Insights />
+          </div> */}
+          <About />
+        </>
+      )}
     </div>
-    <div className="relative">
-      <WhatsNew />
-      <div className="gradient-04 z-0" />
-      <Insights />
-    </div> */}
-    <About />
-  </div>
-);
+  );
+};
 
 export default Page;
