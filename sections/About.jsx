e4526425003f9afe56778f7a30ee08aec246c742
Ayush <ayush4002gupta@gmail.com>
2023-03-13 01:50:00 +0530

    added image in about page

--- sections/About.jsx
+++ sections/About.jsx
@@ -73,13 +73,8 @@ const About = () => (
    
       
         The East Campus, GGSIPU is enthralled to host its first-ever annual Des-Tech fest
-        (ELYSIAN'23) on 27th- 28th March 2023 at its very own celebrated, East
-        Delhi Campus. Design and technology are two fields that are regarded as
-        very different but have always been closely linked with each other, and,
-        their interdependency is clearly obvious. Technology has allowed
-        designers to create innovative products and solutions that were
-        previously deemed to be impossible, while the design has played a vital
-        role in making technology accessible. This first annual DES-TECH event
+        (ELYSIAN'23) at its very own celebrated, East
+        Delhi Campus. This first annual DES-TECH event
         is titled ELYSIAN'23, that refers to "Something that is grand, heavenly,
         blissful a paradise or pure" . It comes from the Greek word 'Elysium', a
         place in Greek mythology that was a kingdom for the spirits of the great
