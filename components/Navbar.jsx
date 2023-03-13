ded2100e076d002b80422cd7215abcd40009fbdc
Ayush Goyal <ayushgoyal8178@gmail.com>
2023-03-14 02:05:37 +0530

    added designathon in mobile

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -41,38 +41,32 @@ const Navbar = () => {
             <div className="ml-10 flex items-center space-x-4">
               <Link
                 href="/"
-                className="text-white hover:text-[#00FFE0] px-3 py-2 rounded-md text-xl font-medium z-20"
-              >
+                className="text-white hover:text-[#00FFE0] px-3 py-2 rounded-md text-xl font-medium z-20">
                 Home
               </Link>
               <Link
                 href="/hackathon"
-                className="text-white hover:text-[#00FFE0] px-3 py-2 rounded-md text-xl font-medium z-20"
-              >
+                className="text-white hover:text-[#00FFE0] px-3 py-2 rounded-md text-xl font-medium z-20">
                 hackEDCode
               </Link>
               <Link
                 href="/design-a-thon"
-                className="text-white hover:text-[#00FFE0] px-3 py-2 rounded-md text-xl font-medium z-20"
-              >
+                className="text-white hover:text-[#00FFE0] px-3 py-2 rounded-md text-xl font-medium z-20">
                 Design-a-Thon
               </Link>
               <Link
                 href="/events"
-                className="text-white hover:text-[#00FFE0] px-3 py-2 rounded-md text-xl font-medium z-20"
-              >
+                className="text-white hover:text-[#00FFE0] px-3 py-2 rounded-md text-xl font-medium z-20">
                 Events
               </Link>
               <Link
                 href="/team"
-                className="text-white hover:text-[#00FFE0] px-3 py-2 rounded-md text-xl font-medium z-20"
-              >
+                className="text-white hover:text-[#00FFE0] px-3 py-2 rounded-md text-xl font-medium z-20">
                 Team
               </Link>
               <Link
                 href="/contact"
-                className="text-white hover:text-[#00FFE0] px-3 py-2 rounded-md text-xl font-medium z-20"
-              >
+                className="text-white hover:text-[#00FFE0] px-3 py-2 rounded-md text-xl font-medium z-20">
                 Contact Us
               </Link>
             </div>
@@ -81,8 +75,7 @@ const Navbar = () => {
         {/* Mobile Button */}
         <div
           onClick={handleNav}
-          className="block sm:hidden z-50 absolute right-7 top-12"
-        >
+          className="block sm:hidden z-50 absolute right-7 top-12">
           {nav ? (
             <AiOutlineClose size={20} style={{ color: `${textColor}` }} />
           ) : (
@@ -95,37 +88,36 @@ const Navbar = () => {
             nav
               ? "z-40 sm:hidden absolute top-0 left-0 right-0 bottom-0 flex justify-center items-center w-full h-screen backdrop-blur-[30px] text-center ease-in duration-300"
               : "z-40 sm:hidden absolute top-0 left-[-100%] right-0 bottom-0 flex justify-center items-center w-full h-screen backdrop-blur-[10px] text-center ease-in duration-300"
-          }
-        >
+          }>
           <ul>
             <li
               onClick={handleNav}
-              className="p-4 z-50 text-4xl text-white hover:text-gray-500"
-            >
+              className="p-4 z-50 text-4xl text-white hover:text-gray-500">
               <Link href="/">Home</Link>
             </li>
             <li
               onClick={handleNav}
-              className="p-4 text-4xl text-white hover:text-gray-500"
-            >
+              className="p-4 text-4xl text-white hover:text-gray-500">
               <Link href="/hackathon">HackEDCode</Link>
             </li>
             <li
               onClick={handleNav}
-              className="p-4 text-4xl text-white hover:text-gray-500"
-            >
+              className="p-4 text-4xl text-white hover:text-gray-500">
+              <Link href="/design-a-thon">Design-a-thon</Link>
+            </li>
+            <li
+              onClick={handleNav}
+              className="p-4 text-4xl text-white hover:text-gray-500">
               <Link href="/events">Events</Link>
             </li>
             <li
               onClick={handleNav}
-              className="p-4 text-4xl text-white hover:text-gray-500"
-            >
+              className="p-4 text-4xl text-white hover:text-gray-500">
               <Link href="/team">Team</Link>
             </li>
             <li
               onClick={handleNav}
-              className="p-4 text-4xl text-white hover:text-gray-500"
-            >
+              className="p-4 text-4xl text-white hover:text-gray-500">
               <Link href="/contact">Contact Us</Link>
             </li>
           </ul>
