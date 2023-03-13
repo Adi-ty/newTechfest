411ead631e746d1b086da6cc47ffc63dc49cad97
lunchspider <lunchspider03@gmail.com>
2023-03-14 01:30:30 +0530

    disable eslint

--- next.config.js
+++ next.config.js
@@ -1,8 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
+    experimental: {
+        appDir: true,
+    },
+    eslint: {
+        // Warning: This allows production builds to successfully complete even if
+        // your project has ESLint errors.
+        ignoreDuringBuilds: true,
+    },
 };
 
 module.exports = nextConfig;
