b4c9aa25bd4a9a96f2f1457b5e05b69e71a6c81f
Ayush Goyal <ayushgoyal8178@gmail.com>
2023-03-14 00:02:20 +0530

    added poster

--- app/hackathon/page.js
+++ app/hackathon/page.js
@@ -275,7 +275,7 @@ const page = () => {
               animate={{ opacity: 1, y: 0 }}
               transition={{ delay: 0.5, duration: 0.8 }}
               className="animate relative rounded-2xl"
-              src="/hacki.png"
+              src="/hackathonnn.png"
               alt=""
             />
           </motion.div>
