735d846873dc8d5ab5c360d6e4e1ec883bb18293
Chaitanya Rai <Chaitanyarai899@gmail.com>
2023-03-14 11:45:42 +0530

    gif aspect ratio improved

--- app/page.js
+++ app/page.js
@@ -33,7 +33,7 @@ const Page = () => {
             position: "fixed",
             top: 0,
             left: 0,
-            width: "90%",
+            width: "100%",
             height: "100%",
             objectFit: "cover",
             zIndex: 9999,
