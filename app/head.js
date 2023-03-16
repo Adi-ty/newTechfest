d0043da2263b7f300f686f92e118a617e78a8fb9
Aditya <adityasinghboss1234@gmail.com>
2023-03-16 07:07:41 +0530

    added metadata

--- app/head.js
+++ app/head.js
@@ -4,8 +4,19 @@ const Head = () => (
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <meta
       name="description"
-      content="Presenting you the first Des-Tech fest at GGSIP University, East Campus on 27th and 28th March."
+      content="Presenting you the first ever des-tech fest at GGSIP University East Campus on 27th and 28th March. Elysian'23 brings you the most exciting hackathon hackEDCode, Design-a-Thon, many exciting events and some great speakers to interact with."
     />
+    <meta
+      name="keywords"
+      content="Des-tech Fest, Elysian'23, hackEDCode, hackathon, tech events"
+    />
+    <meta property="og:title" content="Elysian'23" />
+    <meta
+      property="og:description"
+      content="Presenting you the first ever Des-Tech fest at GGSIP University, East Campus on 27th and 28th March. Elysian'23 brings you the most exciting hackathon hackEDCode, Design-a-Thon, many exciting events and some great speakers to interact with."
+    />
+    <meta property="og:url" content="https://www.elysian-edc.com/" />
+    <meta property="og:type" content="website" />
     <link rel="icon" href="/logo.png" />
   </>
 );
